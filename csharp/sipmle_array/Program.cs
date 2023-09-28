b2171510c47f84aabf54ef9c1f5c92fc76295ff4
Safiya Dziameshka <sophiedemeshko@gmail.com>
2023-09-28 10:56:36 +0300

    my task simple_array

---
+++ csharp/sipmle_array/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace TestApp
+{
+    class MyProgramm
+    {
+        static void Main()
+        {
+            int[] myArray = new int[6];
+            Console.WriteLine("Enter elements of myArray");
+            for (int i = 0; i < 6; i++)
+            {
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("See the elements of myArray");
+            for (int i = 0; i < myArray.Length; i++) { Console.WriteLine($"myarray[{i}] = {myArray[i]}"); }
+
+            myArray[2] *= 10;
+            Console.WriteLine("See new values of elements in myArray");
+            for (int i = 0; i < myArray.Length; i++) { Console.WriteLine($"myarray[{i}] = {myArray[i]}"); }
+
+        }
+    }
+}
+
