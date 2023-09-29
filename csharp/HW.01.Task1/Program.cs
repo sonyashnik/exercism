9c5c9cba62583d9f9ad418d3c5d33984199fd306
sonyashnik <cheeky-chicken@mail.ru>
2023-09-29 18:28:09 +0300

    Task 1 and Task 2

---
+++ csharp/HW.01.Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a dimension of arrays:");
+int i = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = new int[i];
+Random random = new Random();
+for (int j = 0; j < i; j++)
+{
+    array1[j] = random.Next(1, i * 10);
+}
+
+int[] array2 = new int[i];
+for  (int j = 0;j<i; j++)
+{
+    Console.WriteLine($"Enter array2[{j}]:");
+    array2[j] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] array3 = new int[i];
+for (int j = 0;j<i; j++)
+{
+    array3[j] = array1[j] + array2[j];
+}
+
+Console.WriteLine("array1:");
+foreach (int element in array1) Console.Write($"{element} ");
+Console.WriteLine("");
+Console.WriteLine("array2:");
+foreach (int element in array2) Console.Write($"{element} ");
+Console.WriteLine("");
+Console.WriteLine("array3:");
+foreach (int element in array3) Console.Write($"{element} ");
---
+++ csharp/HW.01.Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[] array = new int[10];
+for (int i = 0; i < array.Length-1; i++)
+{
+    Console.Write($"array[{i}] = ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("array = { ");
+foreach (int element in array) Console.Write(element+" ");
+Console.WriteLine("} ");
+
+Console.Write("Enter one more value: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Enter the index in array from 0 to {array.Length - 1}: ");
+int index  = Convert.ToInt32(Console.ReadLine());
+
+for (int i = array.Length-1; i > index; i--)
+{
+    array[i] = array[i-1];
+}
+array[index] = value;
+
+Console.Write("new array = { ");
+foreach (int element in array) Console.Write(element + " ");
+Console.Write("} ");
