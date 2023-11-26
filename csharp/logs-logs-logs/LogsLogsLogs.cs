d776c17b02b25130326e468d20ab821615f78d3c
sonyashnik <cheeky-chicken@mail.ru>
2023-11-26 13:44:53 +0400

    LogLevels exercism

---
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -0,0 +1,34 @@
+using System;
+
+public enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal =42
+}
+
+static class LogLine
+{
+    public static LogLevel ParseLogLevel(string logLine)
+    {
+        switch (logLine.Substring(1,3))
+        {
+            case "TRC": return LogLevel.Trace;
+            case "DBG": return LogLevel.Debug;
+            case "INF": return LogLevel.Info;
+            case "WRN": return LogLevel.Warning;
+            case "ERR": return LogLevel.Error;
+            case "FTL": return LogLevel.Fatal;
+            default: return LogLevel.Unknown;
+        }
+    }
+
+    public static string OutputForShortLog(LogLevel logLevel, string message)
+    {
+        return $"{(int)logLevel}:{message}";
+    }
+}
