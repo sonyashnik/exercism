ec1e514ae23c467e2ba8b86bb02b37c797fcca85
sonyashnik <cheeky-chicken@mail.ru>
2023-12-04 20:23:35 +0400

    Classes Shape, Circle, Triangle and Square
    Homework

---
+++ csharp/HW.02.Task1/Program.cs
@@ -0,0 +1,70 @@
+﻿
+for (int i = 0; i < 5; i++)
+{
+    var rnd = new Random();
+    switch (rnd.Next(0, 3))
+    {
+        case 0:
+            bool radIsValid = false;
+            int r = default;
+            while (!radIsValid)
+            {
+                Console.Write("Enter a radius of the circle: ");
+                if (int.TryParse(Console.ReadLine(), out r))
+                {
+                    radIsValid = true;
+                }
+            }
+            var crcl = new Circle(r);
+            Console.WriteLine(crcl.ShapeInfo());
+            Console.WriteLine();
+            break;
+        case 1:
+            bool sideIsValid = false;
+            int s = default;
+            while (!sideIsValid)
+            {
+                Console.Write("Enter a side of the square: ");
+                if (int.TryParse(Console.ReadLine(), out s))
+                {
+                    sideIsValid = true;
+                }
+            }
+            var sqr = new Square(s);
+            Console.WriteLine(sqr.ShapeInfo());
+            Console.WriteLine();
+            break;
+        case 2:
+            bool heightIsValid = false;
+            bool baseIsValid = false;
+            int h = default;
+            int b = default;
+            while (!heightIsValid)
+            {
+                Console.Write("Enter a height of the triangle: ");
+                if (int.TryParse(Console.ReadLine(), out h))
+                {
+                    heightIsValid = true;
+                }
+            }
+            while (!baseIsValid)
+            {
+                Console.Write("Enter a base of the triangle: ");
+                if (int.TryParse(Console.ReadLine(), out b))
+                {
+                    baseIsValid = true;
+                }
+            }
+            var trngl = new Triangle(h, b);
+            Console.WriteLine(trngl.ShapeInfo());
+            break;
+        default:
+            Console.WriteLine("No shape was found!");
+            break;
+
+
+    }
+
+}
+
+
---
+++ csharp/HW.02.Task1/Shape.cs
@@ -0,0 +1,65 @@
+﻿public class Shape
+{
+    protected  string _name { get; set; }
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public virtual double GetSquare()
+    {
+        return 0;
+    }
+    public string ShapeInfo()
+    {
+        return $"This is {this.Name}. CLR Type is {this.GetType()}. Square is {this.GetSquare()}.";
+    }
+}
+public class Square : Shape
+{
+    private int Side { get; set; }
+    public Square(int s)
+    {
+        Side = s;
+        this.Name = "square";
+    }
+    public override double GetSquare()
+    {
+        return Math.Pow(Side, 2);
+    }
+}
+
+public class Circle : Shape
+{
+    private int Radius { get; set; }
+  public Circle (int @radius)
+    {
+        Radius = @radius;
+        this.Name = "circle";
+    }
+
+    public override double GetSquare()
+    {
+        return Math.Round(Math.PI * Math.Pow(Radius, 2), 2);
+    }
+
+}
+
+public class Triangle : Shape
+{
+    private int Height { get; set; }
+    private int Base { get; set; }
+
+    public Triangle(int @height, int @base)
+    {
+        Height = @height;
+        Base = @base;
+        this.Name = "triangle";
+    }
+
+    public override double GetSquare()
+    {
+        return Math.Round((double)(Base * Height / 2d), 2);
+    }
+}
