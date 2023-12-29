c11412ad2ddd21912eeb08a11ab7453cad8032a6
sonyashnik <cheeky-chicken@mail.ru>
2023-12-29 13:36:03 +0400

    Task 2 Neighbor Class, List

---
+++ csharp/HW.4.Task.2.Neighbor.List/Neighbor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._4.Task._2.Neighbor.List
+{
+    internal class Neighbor
+    {
+        private string _fullName;
+        public int FlatNumber;
+        private string _phoneNumber;
+
+        public Neighbor(string fullName, int flatNumber, string phoneNumber)
+        {
+            _fullName = fullName;
+            FlatNumber = flatNumber;
+            _phoneNumber = phoneNumber;
+        }
+        public override string ToString()
+        {
+            return $"{_fullName}, phone: {_phoneNumber}";
+        }
+    }
+}
