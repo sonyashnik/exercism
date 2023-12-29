98f9b28b1e60ee3e680a493cf1970a20106f3469
sonyashnik <cheeky-chicken@mail.ru>
2023-12-29 11:54:22 +0400

    class StringHelper
    Extension method for String, that cuts first 5 symbols and adds three dots.

---
+++ csharp/HW.4.Task.1.String.Extension/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._4.Task._1.String.Extension
+{
+    static class StringHelper
+    {
+        public static string GetString(this string str) 
+        { 
+            return $"{str.Substring(0,5)}...";
+        }
+    }
+}
