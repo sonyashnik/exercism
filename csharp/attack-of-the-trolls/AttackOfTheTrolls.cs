be7612f097badab73cc4461bd7e9a61e3362e170
sonyashnik <cheeky-chicken@mail.ru>
2024-02-04 21:55:07 +0400

    Add exercism projects to one solution

--- csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
+++ csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -1,28 +1,45 @@
 using System;
-
-// TODO: define the 'AccountType' enum
-
-// TODO: define the 'Permission' enum
-
+enum AccountType
+{
+    Guest,
+    User,
+    Moderator
+}
+[Flags]
+enum Permission
+{
+    None = 0,
+    Read = 1,
+    Write = 2,
+    Delete = 4,
+    All = Read | Write | Delete
+}
 static class Permissions
 {
     public static Permission Default(AccountType accountType)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Default() method");
+        switch (accountType)
+        {
+            case AccountType.Guest:
+                return Permission.Read;
+            case AccountType.User:
+                return Permission.Read | Permission.Write;
+            case AccountType.Moderator:
+                return Permission.All;
+            default:
+                return Permission.None;
+        }
     }
-
     public static Permission Grant(Permission current, Permission grant)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Grant() method");
+        return current | grant;
     }
-
     public static Permission Revoke(Permission current, Permission revoke)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Revoke() method");
+        return (current & ~revoke);
     }
-
     public static bool Check(Permission current, Permission check)
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Check() method");
+        return (current & check) == check ? true : false;
     }
 }
