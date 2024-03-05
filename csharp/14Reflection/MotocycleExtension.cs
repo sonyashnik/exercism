72fc41165e826a4f167d7010e26bd40a4ed60553
sonyashnik <cheeky-chicken@mail.ru>
2024-03-05 17:03:26 +0400

    reflection tests

---
+++ csharp/14Reflection/MotocycleExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14Reflection
+{
+	static class MotocycleExtension
+	{
+		public static void TakeWheelie(this Motorcycle moto, int acceleration)
+		{
+			moto.Acceleration = acceleration;
+			moto.Move(10);
+		}
+	}
+}
---
+++ csharp/14Reflection/Motorcycle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14Reflection
+{
+    internal class Motorcycle
+    {
+        public const string motoState = "new";
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Acceleration { get; set; }
+
+        public int Distance { get; set; }
+
+        private int _vinNumber = 111;
+
+        int _odometer;
+        public int Odometer { get => _odometer; set => _odometer = value; }
+
+        public Motorcycle()
+        {
+        }
+
+        public Motorcycle(string name)
+        {
+            Name = name;
+        }
+
+        public Motorcycle(System.DateTime created, string name)
+        {
+        }
+
+        public int GetVinNumber()
+        {
+            return _vinNumber;
+        }
+        public void Move(int km)
+        {
+            Acceleration = 1;
+            Distance += km;
+        
+        }
+
+        public override string ToString()
+        {
+            return $"Motorcycle: {Name}, Model: {Model}, Odometer: {Odometer}, VinNumber: {GetVinNumber()}, State: {motoState}";
+        }
+
+        class TechnicalInfo { };
+    }
+}
