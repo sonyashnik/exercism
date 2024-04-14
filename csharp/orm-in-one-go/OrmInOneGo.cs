f4e897dc62d92f1be11acc1ae7b1e1d983180e4e
sonyashnik <cheeky-chicken@mail.ru>
2024-04-14 19:37:15 +0400

    orm

---
+++ csharp/orm-in-one-go/OrmInOneGo.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Data;
+
+public class Orm
+{
+    private Database database;
+
+    public Orm(Database database)
+    {
+        this.database = database;
+    }
+
+    public void Write(string data)
+    {
+        using (database)
+        {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+    }
+
+    public bool WriteSafely(string data)
+    {
+        using (database) 
+        {
+            try
+            {
+                database.BeginTransaction();
+                database.Write(data);
+                database.EndTransaction();
+                return true;
+            }
+            catch (InvalidOperationException)
+            {
+                database.Dispose();
+                return false;
+            }
+         
+        }
+	}
+}
