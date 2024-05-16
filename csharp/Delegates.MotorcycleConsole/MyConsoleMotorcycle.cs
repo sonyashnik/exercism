739d1318b4c6142c10b7ac1fe94578613ef53e4b
sonyashnik <cheeky-chicken@mail.ru>
2024-05-16 22:17:37 +0400

    delegates

--- csharp/Delegates.MotorcycleConsole/MyConsoleMotorcycle.cs
+++ csharp/Delegates.MotorcycleConsole/MyConsoleMotorcycle.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Delegates.GlobalMotorcycleServiceModule;
 
 namespace Delegates.MotorcycleConsole
 {
@@ -10,5 +11,30 @@ namespace Delegates.MotorcycleConsole
 	{
         public string Model { get; set; }
         public static int Odometer { get; set; }       
+
+        public int DailyDistance { get; set; }
+
+        public MyConsoleMotorcycle(string model)
+        {
+            Model = model;
+        }
+
+        public void StartEngine() 
+        {
+            Console.WriteLine();
+            Console.WriteLine("Engine started.");
+        }
+        public void Move(int distance)
+        {
+            DailyDistance += distance;
+            Console.WriteLine($"Move to {distance} km.");
+        }
+        public void StopEngine() 
+        { 
+            Odometer += DailyDistance;
+            MotorcycleService.TotalDistance += DailyDistance;
+            Console.WriteLine("Engine stoped."); 
+            Console.WriteLine($"Total distance: {Odometer} km."); 
+        }
     }
 }
