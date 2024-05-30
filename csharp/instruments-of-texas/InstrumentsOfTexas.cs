1fe163bf6396a49f158e8245504c101f7da5f82c
sonyashnik <cheeky-chicken@mail.ru>
2024-05-30 14:43:52 +0400

    Calculator

--- csharp/instruments-of-texas/InstrumentsOfTexas.cs
+++ csharp/instruments-of-texas/InstrumentsOfTexas.cs
@@ -2,33 +2,53 @@ using System;
 
 public class CalculationException : Exception
 {
-    public CalculationException(int operand1, int operand2, string message, Exception inner)
-    // TODO: complete the definition of the constructor
-    {
-    }
+	public CalculationException(int operand1, int operand2, string message, Exception inner) : base(message, inner)
+	{
+		Operand1 = operand1;
+		Operand2 = operand2;
+	}
 
-    public int Operand1 { get; }
-    public int Operand2 { get; }
+	public int Operand1 { get; }
+	public int Operand2 { get; }
 }
 
 public class CalculatorTestHarness
 {
-    private Calculator calculator;
+	private Calculator calculator;
 
-    public CalculatorTestHarness(Calculator calculator)
-    {
-        this.calculator = calculator;
-    }
+	public CalculatorTestHarness(Calculator calculator)
+	{
+		this.calculator = calculator;
+	}
 
-    public string TestMultiplication(int x, int y)
-    {
-        throw new NotImplementedException("Please implement the CalculatorTestHarness.TestMultiplication() method");
-    }
+	public string TestMultiplication(int x, int y)
+	{
+		try
+		{
+			Multiply(x, y);
+			return "Multiply succeeded";
+		}
+		catch (CalculationException cex) when (cex.Operand1 < 0 && cex.Operand2 < 0)
+		{
+			return "Multiply failed for negative operands. " + cex.InnerException.Message;
+		}
+		catch (CalculationException cex)
+		{
+			return "Multiply failed for mixed or positive operands. " + cex.InnerException.Message;
+		}
+	}
 
-    public void Multiply(int x, int y)
-    {
-        throw new NotImplementedException("Please implement the CalculatorTestHarness.Multiply() method");
-    }
+	public void Multiply(int x, int y)
+	{
+		try
+		{
+			calculator.Multiply(x, y);
+		}
+		catch (OverflowException ofex)
+		{
+			throw new CalculationException(x, y, string.Empty, ofex);
+		}
+	}
 }
 
 
@@ -37,11 +57,11 @@ public class CalculatorTestHarness
 // then a System.OverflowException is thrown.
 public class Calculator
 {
-    public int Multiply(int x, int y)
-    {
-        checked
-        {
-            return x * y;
-        }
-    }
+	public int Multiply(int x, int y)
+	{
+		checked
+		{
+			return x * y;
+		}
+	}
 }
