fae52140f6f3b2a0503774da00357e1ce606cb8a
sonyashnik <cheeky-chicken@mail.ru>
2024-06-17 21:19:28 +0400

    NucleotideCount

---
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class NucleotideCount
+{
+    public static IDictionary<char, int> Count(string sequence)
+    {
+        char[] nucleotides = { 'A', 'C', 'G', 'T' };
+        if (sequence.Except(nucleotides).Any())  throw new ArgumentException("");
+        return new Dictionary<char, int>(){
+            {'A', sequence.Count(a => a == 'A') },
+            {'C', sequence.Count(c => c == 'C') },
+            {'G', sequence.Count(g => g == 'G') },
+            {'T', sequence.Count(t => t == 'T') }
+        };
+    }
+}
