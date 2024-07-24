a5c22fca5d3dc9362bba291cee529328484c88c7
sonyashnik <cheeky-chicken@mail.ru>
2024-07-24 23:00:58 +0400

    circular buffer

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -1,29 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class CircularBuffer<T>
 {
+    private readonly int _capacity;
+    private List<T> _buffer;
+
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        _capacity = capacity;
+        _buffer = new List<T>(capacity);
     }
-
+    private void Shift() => _buffer = _buffer.Skip(1).ToList();
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        if (_buffer.Count() == 0)
+        {
+            throw new InvalidOperationException("Cannot read from empty buffer");
+        }
+        else 
+        {
+            T value = _buffer[0];
+            Shift();
+            return value;
+        } 
     }
 
-    public void Write(T value)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+        public void Write(T value)
+        {
+            if (_buffer.Count() != _capacity)
+            {
+                _buffer.Add(value);
+            }
+            else
+            {
+                throw new InvalidOperationException("Cannot write to full buffer");
+            }
+        }
 
-    public void Overwrite(T value)
-    {
-        throw new NotImplementedException("You need to implement this method.");
-    }
+        public void Overwrite(T value)
+        {
+            if (_buffer.Count() == _capacity) Shift();
+            _buffer.Add(value);
+        }
 
-    public void Clear()
-    {
-        throw new NotImplementedException("You need to implement this method.");
+        public void Clear()
+        {
+            _buffer.Clear();
+        }
     }
-}
