9adcd57230b6facb078d3936b4bb8c7005016e32
sonyashnik <cheeky-chicken@mail.ru>
2024-08-05 18:51:53 +0400

    Bob

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,22 @@
-using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        switch (true) {
+            case true when IsShout(statement) && IsQuestion(statement):
+                return "Calm down, I know what I'm doing!";
+            case true when IsShout(statement):
+                return "Whoa, chill out!";
+            case true when IsQuestion(statement):
+                return "Sure.";
+            case true when string.IsNullOrWhiteSpace(statement):
+                return "Fine. Be that way!";
+            default:
+                return "Whatever.";
+        }
     }
+    public static bool IsShout(string input) => input.Any(char.IsLetter) && input.ToUpper() == input ;
+    public static bool IsQuestion(string input) =>input.TrimEnd().EndsWith('?');
 }
