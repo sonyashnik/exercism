3cccba1c41500f719c20ab9654d4ce712404b49c
sonyashnik <cheeky-chicken@mail.ru>
2024-08-07 10:51:10 +0400

    tournament

--- csharp/spiral-matrix/SpiralMatrix.cs
+++ csharp/spiral-matrix/SpiralMatrix.cs
@@ -4,6 +4,27 @@ public class SpiralMatrix
 {
     public static int[,] GetMatrix(int size)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var matrix = new int[size, size];
+
+        var rowOffset = 0;
+        var columnOffset = 0;
+        var totalNumbers = size * size;
+        var currentNumber = 1;
+        while (currentNumber <= totalNumbers)
+        {
+            for (var topColumn = columnOffset; topColumn < size - columnOffset; topColumn++)
+                matrix[rowOffset, topColumn] = currentNumber++;
+
+            for (var rightRow = rowOffset + 1; rightRow < size - rowOffset; rightRow++)
+                matrix[rightRow, size - columnOffset - 1] = currentNumber++;
+            for (var bottomColumn = size - columnOffset - 2; bottomColumn >= columnOffset; bottomColumn--)
+                matrix[size - rowOffset - 1, bottomColumn] = currentNumber++;
+
+            for (var leftRow = size - rowOffset - 2; leftRow >= rowOffset + 1; leftRow--)
+                matrix[leftRow, columnOffset] = currentNumber++;
+            rowOffset += 1;
+            columnOffset += 1;
+        }
+        return matrix;
     }
 }
